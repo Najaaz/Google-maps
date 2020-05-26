8093ab543859ff56b885dbf66f286cbb09b50ac5
Najaaz Nabhan <najaaznabhan@gmail.com>
2020-05-26 06:12:14 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function initMap(){
+    var sydney = {lat:6.9271 , lng:79.8612};
+    map = new google.maps.Map(document.getElementById('map'),{
+        center: sydney,
+        zoom: 10,
+        mapTypeId: 'roadmap',
+    });
+}
+
+function showStoreMarkers(){
+
+
+}
+
+
+function createMarker (latlng, name , address){
+
+}
