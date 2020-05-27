e42f6b8820ccdfb1d5bd79a990e8b9b099f87633
Najaaz Nabhan <najaaznabhan@gmail.com>
2020-05-27 05:54:43 +0530

    Added the details of stores in the app

--- js/index.js
+++ js/index.js
@@ -1,18 +1,77 @@
+var markers =[]
+var infoWindow;
+var locationSelect;
+
+
 function initMap(){
-    var sydney = {lat:6.9271 , lng:79.8612};
+    var cmb = {lat:6.8742 , lng:79.8612};
     map = new google.maps.Map(document.getElementById('map'),{
-        center: sydney,
-        zoom: 10,
+        center: cmb,
+        zoom: 16,
         mapTypeId: 'roadmap',
     });
+
+    infoWindow = new google.maps.InfoWindow();
+    displayStores()
+    showStoreMarkers()
 }
 
 function showStoreMarkers(){
+    var bounds = new google.maps.LatLngBounds();
+    stores.forEach(function(store ,index){
+        var latlng = new google.maps.LatLng(
+            store.coordinates.latitude,
+            store.coordinates.longitude);
 
+        var name = store.name
+        var address = store.address.streetAddressLine1
 
+        createMarker(latlng , name , address, index)
+        bounds.extend(latlng)
+    })
+    map.fitBounds(bounds);
 }
 
 
-function createMarker (latlng, name , address){
+function createMarker (latlng, name , address , index){
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+        label: `${index+1}`,
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+    });
+    markers.push(marker);
+}
 
+
+function displayStores(){
+    var storesHtml = ''
+    stores.forEach(function(stores , index){
+        var address = stores.address
+        var phonenumber = stores.PhoneNumber
+
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-container-background">
+                    <div class="store-info-container">
+                        <div class="store-address">
+                            <span>${address.streetAddressLine1}</span>
+                            <span>${address.streetAddressLine2}</span>
+                        </div>
+                        <div class="store-phone-number">${phonenumber}</div>
+                    </div>
+                    <div class="store-number-container">
+                        <div class="store-number">${index+1}</div>
+                    </div>
+                </div>
+            </div>
+            
+        `
+    })
+
+    document.querySelector(".stores-list").innerHTML = storesHtml
 }
---
+++ js/store-data.js
@@ -0,0 +1,45 @@
+let stores = [{
+    "recommendation":{},
+    "storeNumber":"2432",
+    "id":"001",
+    "name":"Arpico SuperCenter",
+    "PhoneNumber":"0772548",
+    "coordinates":{
+        "latitude":6.869050,
+        "longitude":79.862195,
+    },
+    "regulations":[],
+    "address":{
+        "streetAddressLine1":"No. 43, Galle Road",
+        "streetAddressLine2":"Wellewatte, SL",
+        "streetAddressLine3":null,
+        "city":"Colombo",
+        "countrySubdivisionCode": "CMB",
+        "countryCode": "LK",
+        "postalCode":"000600"
+    }
+},
+
+    {
+    "recommendation":{},
+    "storeNumber":"2433",
+    "id":"002",
+    "name":"Cargills Food City",
+    "PhoneNumber":"+94 225 7854",
+    "coordinates":{
+        "latitude":6.871136,
+        "longitude":79.861824,
+    },
+    "regulations":[],
+    "address":{
+        "streetAddressLine1":"No. 22, Galle Road",
+        "streetAddressLine2":"Wellewatte, SL",
+        "streetAddressLine3":null,
+        "city":"Colombo",
+        "countrySubdivisionCode": "CMB",
+        "countryCode": "LK",
+        "postalCode":"000600"
+
+    }
+},
+]
